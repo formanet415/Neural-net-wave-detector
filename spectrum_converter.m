a83df56cd4853c3f7aed80c2f8d01b944175a0cc
formanet420 <t.formanek@outlook.cz>
2021-05-06 21:16:02 +0200

    Update spectrum_converter.m
    Now works with twice as long measurements (TSWF usually uses that)

--- spectrum_converter.m
+++ spectrum_converter.m
@@ -15,7 +15,7 @@ end
 cdf = cdf_load_tswf(CDF_DIR);
 
 for i=1:length(cdf.WAVEFORM_DATA.data(1,1,:))
-    if cdf.SAMPS_PER_CH.data(i,1) == 8192 
+    if cdf.SAMPS_PER_CH.data(i,1) == 8192
         
         fs = cdf.SAMPLING_RATE.data(i,1);
         data = convert_to_SRF(cdf, i);
@@ -31,7 +31,48 @@ for i=1:length(cdf.WAVEFORM_DATA.data(1,1,:))
         %sp = rescale(abs(sp),0,255);
         
         disp(sprintf('0_%u.png', i))
-        imwrite(sp,fullfile('converted_spectra',sprintf('%u.png',i)))
+        imwrite(sp,fullfile('converted_tswf_spectra',sprintf('%u.png',i)))
+        
+    end
+    if cdf.SAMPS_PER_CH.data(i,1) == 8192*2
+        
+        fs = cdf.SAMPLING_RATE.data(i,1);
+        data = convert_to_SRF(cdf, i);
+        
+        temp = size(data);
+        
+        data1 = data(:,1:temp(2)/2);
+        data2 = data(:,temp(2)/2+1:end);
+        
+        data = data1;
+        
+        sp1 = spectrogram(data(1,:), 64, 0, 200, fs, "yaxis");
+        sp2 = spectrogram(data(2,:), 64, 0, 200, fs, "yaxis");
+        sp = abs(sp1)+abs(sp2);
+        rowmin = min(sp, [],2);
+        rowmax = max(sp, [],2);
+        sp = rescale(sp,0,1);%,'InputMin',rowmin,'InputMax',rowmax);
+        [n,m] = size(sp);
+        sp = sp(fix(n*0.6):-1:1,:);
+        %sp = rescale(abs(sp),0,255);
+        
+        disp(sprintf('0_%u.png', i))
+        imwrite(sp,fullfile('converted_tswf_spectra',sprintf('%u_1.png',i)))
+        
+        data = data2;
+        
+        sp1 = spectrogram(data(1,:), 64, 0, 200, fs, "yaxis");
+        sp2 = spectrogram(data(2,:), 64, 0, 200, fs, "yaxis");
+        sp = abs(sp1)+abs(sp2);
+        rowmin = min(sp, [],2);
+        rowmax = max(sp, [],2);
+        sp = rescale(sp,0,1);%,'InputMin',rowmin,'InputMax',rowmax);
+        [n,m] = size(sp);
+        sp = sp(fix(n*0.6):-1:1,:);
+        %sp = rescale(abs(sp),0,255);
+        
+        disp(sprintf('0_%u.png', i))
+        imwrite(sp,fullfile('converted_tswf_spectra',sprintf('%u_2.png',i)))
         
     end
 end
