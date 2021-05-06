7bd1d8bcf0b3c29de9d7cae7642aae429174fbdf
formanet420 <t.formanek@outlook.cz>
2021-05-06 21:20:01 +0200

    better wave finder

--- wave_finder.py
+++ wave_finder.py
@@ -6,21 +6,26 @@ from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from PIL import Image
 import numpy as np
+from pathlib import Path
 
 loaded_model = keras.models.load_model('saved_modelv2')
 print(loaded_model.summary())
 
-directory = r'converted_spectra/'
-for filename in os.listdir(directory):
-    if filename.endswith(".jpg") or filename.endswith(".png"):
-        print(os.path.join(directory, filename))
+directory = r'converted_tswf_spectra/'
+paths = sorted(Path(directory).iterdir(), key=os.path.getmtime)
+for filename in paths:
+    if str(filename).endswith(".jpg") or str(filename).endswith(".png"):
+        #print(os.path.join(directory, filename))
         #evaluating         Input(shape=(128, 60, 1))
 
-        image = tf.keras.preprocessing.image.load_img(os.path.join('converted_spectra', filename), color_mode='grayscale')
+        image = tf.keras.preprocessing.image.load_img(str(filename), color_mode='grayscale')
         input_arr = keras.preprocessing.image.img_to_array(image)
         input_arr = np.array([input_arr])  # Convert single image to a batch.
         predictions = loaded_model.predict(input_arr)
-        print(predictions)
+        #print(predictions)
+        if (0.1<predictions[0,1]):
+            print(str(filename) + ' %f chance of being ESW' %predictions[0,1])
+            print()
         #print(loaded_model(input_arr))
     else:
         continue
