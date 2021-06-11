c943b12b6e2b23805158123c76fc9c0d2abc343c
Tom <58693041+formanet420@users.noreply.github.com>
2021-06-11 21:19:10 +0200

    Generated training data
    data  where there are no waves or (hopefully) dust
    (lots of empty data, added to gitignore because it was 301MB)

--- dataset_generator_V02.py
+++ dataset_generator_V02.py
@@ -11,8 +11,8 @@ target = np.array([])
 
 ##      LOADING CDFs WITH ESW       ##
 #esw_tswf = cdf.load_rpw('tswf', 2020, 12, 27, 'nnet')
-#esw_rswf = cdf.load_rpw('rswf', 2020, 12, 27, 'nnet')
-dst_tswf = cdf.load_rpw('tswf', 2021,  1, 10, 'nnet')
+esw_rswf = cdf.load_rpw('rswf', 2020, 12, 27, 'nnet')
+#dst_tswf = cdf.load_rpw('tswf', 2021,  1, 10, 'nnet')
 #cdfs = [esw_tswf]  # ,esw_rswf,dst_tswf]
 
 ##      LOADING THE LABELS          ##
@@ -22,7 +22,7 @@ tdst_txt = np.array(pd.read_csv('./data/indexes_tswf_20210110_DST.txt').columns)
 indxs = [tesw_txt]
 
 
-##      Function for editing labels     ##
+##      Function for editing labels     ## requires manual prompting
 def relabel(cdf, indexes):
     lngth = 4096  # desired length after relabeling
     nsamp = cdf['SAMPS_PER_CH']
@@ -68,6 +68,45 @@ def relabel(cdf, indexes):
 
 
 #relabel(dst_tswf, tdst_txt)
+
+## SAVING REST OF LABELED FILE (Function) ## Runs on its own
+
+def save_unlabeled(cdf,txt,limit):
+    nsamp = cdf['SAMPS_PER_CH']
+    lngth = 4096
+    for i in range(limit):
+        if i not in txt:
+            ns = nsamp[i]
+            parts = int(ns / 4096)
+            print('snapshot %d has %d parts' % (i, parts))
+
+            w = np.append(SRF.convert(cdf, i), np.array([[0], [0]]), axis=1)
+            snap_data = np.zeros([2, parts, lngth])
+
+            for j in range(parts):
+                snap_data[:, j, :] = w[:, j * lngth:(j + 1) * lngth]
+            if 'out_data' not in locals():
+                out_data = snap_data
+            else:
+                out_data = np.append(out_data, snap_data, axis=1)
+
+            snap_label = np.zeros(parts).astype(int)
+            if 'out_label' not in locals():
+                out_label = snap_label
+            else:
+                out_label = np.append(out_label, snap_label)
+            del snap_label
+
+    with open('empty_rswf_data.pickle', 'wb') as f:
+        pickle.dump(out_data, f)
+
+    with open('empty_rswf_label.pickle', 'wb') as f:
+        pickle.dump(out_label, f)
+
+    print('saved')
+
+save_unlabeled(esw_rswf, resw_txt)
+
 #nsamp = esw_tswf['SAMPS_PER_CH']
 #for i in range(len(nsamp)):
 #    pass
